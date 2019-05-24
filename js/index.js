54338741a0f57ebcb703bb0c2530c3f57e18a358
naaspati <naaspati@gmail.com>
2019-05-24 18:22:08 +0530

    minor

--- js/index.js
+++ js/index.js
@@ -30,6 +30,11 @@ function setError(name, msgOnError) {
     document.getElementById(name.concat('_error')).innerHTML = msgOnError
 }
 
+const FIRST_NAME = 0,
+    LAST_NAME = 1,
+    PHONE = 2,
+    EMAIL = 3;
+
 const form_names = ['first_name', 'last_name', 'phone', 'email'];
 const form_lebels = ['First Name', 'Last Name', 'Phone number', 'Email address'];
 const validations = [];
@@ -37,21 +42,22 @@ const validations = [];
 function addValidation(index, validator, errorFormatter) {
     validations.push(() => validateField(form_names[index], errorFormatter(form_lebels[index], form_names[index]), validator));
 }
-function addEmptyCheck(index) {
+function addCheck(index, min_len, max_len) {
     addValidation(index, s => !isEmpty(s), s => `${s} cannot be empty`);
-}
-function addBasicCheck(index, min_len, max_len) {
-    addEmptyCheck(index);
-    addValidation(index, s => !isShort(s, min_len), s => `${s} cannot be shorter than ${min_len} charators`);
-    addValidation(index, s => !isLong(s, max_len), s => `${s} cannot be longer than ${max_len} charators`);
+
+    if (min_len || min_len === 0)
+        addValidation(index, s => !isShort(s, min_len), s => `${s} cannot be shorter than ${min_len} charators`);
+    if (max_len || max_len === 0)
+        addValidation(index, s => !isLong(s, max_len), s => `${s} cannot be longer than ${max_len} charators`);
 }
 
-addBasicCheck(0, 3, 32);
-addBasicCheck(1, 3, 32);
-addBasicCheck(2, 10, 13);
+//Magic Numbers
+addCheck(FIRST_NAME, 3, 32);
+addCheck(LAST_NAME, 3, 32);
+addCheck(PHONE, 10, 13);
 
-addEmptyCheck(3);
-addValidation(3, email => !(/^\w{3,}@\w{3,}\\.\w{2,4}$/.test(email)), () => "invalid email address");
+addCheck(EMAIL);
+addValidation(EMAIL, email => !(/^\w{3,}@\w{3,}\\.\w{2,4}$/.test(email)), () => "invalid email address");
 
 let errorSpans;
 function clear() {
